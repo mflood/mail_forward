f5710b87c4c732ba082d3fa26888f2876a847a2b
Matthew Flood <hermannwest@gmail.com>
2019-05-16 06:59:12 -0600

    hookup logging in main app

--- flask_app/mail_forward_flask/__init__.py
+++ flask_app/mail_forward_flask/__init__.py
@@ -1,10 +1,26 @@
+"""
+    Flask App for mail_forward
+"""
+import logging
 from flask import Flask
 from flask import render_template
+from mail_forward_flask.loggingsetup import APP_LOGNAME
+from mail_forward_flask.loggingsetup import init_logging
 
+# lowercase app is standard name for flask app
+# pylint: disable=invalid-name
 app = Flask(__name__)
+
+# setup logging
+init_logging(logging.DEBUG)
+logger = logging.getLogger(APP_LOGNAME)
 
 @app.route("/")
 def index():
+    """
+        endpoint for app main page
+    """
+    logger.debug("Rendering index.html")
     return render_template('index.html')
 
 if __name__ == "__main__":
