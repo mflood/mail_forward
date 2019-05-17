577a2d064ef0d4bb48b856a2eb82bedc00c4f183
Matthew Flood <hermannwest@gmail.com>
2019-05-17 12:03:20 -0600

    Add bad message to run_curl.sh

--- flask_app/run_curl.sh
+++ flask_app/run_curl.sh
@@ -1,1 +1,11 @@
-curl -d '{ "to": "fake@example.com", "to_name": "Mr. Fake", "from": "noreply@mybrightwheel.com", "from_name": "Brightwheel", "subject": "A Message from Brightwheel", "body": "<h1>Your Bill</h1><p>$10</p>"}' -H "Content-Type: application/json" -X POST http://127.0.0.1:5000/email
+# for flask / gunicorn
+PORT=5000
+
+# for Docker:
+#PORT=8000
+
+# normal message
+curl -d '{ "to": "fake@example.com", "to_name": "Mr. Fake", "from": "noreply@mybrightwheel.com", "from_name": "Brightwheel", "subject": "A Message from Brightwheel", "body": "<h1>Your Bill</h1><p>$10</p>"}' -H "Content-Type: application/json" -X POST http://127.0.0.1:$PORT/email
+
+# bad email message
+curl -d '{ "to": "bademail", "to_name": "Mr. Fake", "from": "noreply@mybrightwheel.com", "from_name": "Brightwheel", "subject": "A Message from Brightwheel", "body": "<h1>Your Bill</h1><p>$10</p>"}' -H "Content-Type: application/json" -X POST http://127.0.0.1:$PORT/email
