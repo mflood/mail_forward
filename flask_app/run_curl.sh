c8816bbce0d5200dc15f59b604af814195319b78
Matthew Flood <hermannwest@gmail.com>
2019-05-17 12:34:38 -0600

    add more curl

--- flask_app/run_curl.sh
+++ flask_app/run_curl.sh
@@ -5,7 +5,10 @@ PORT=5000
 #PORT=8000
 
 # normal message
-curl -d '{ "to": "fake@example.com", "to_name": "Mr. Fake", "from": "noreply@mybrightwheel.com", "from_name": "Brightwheel", "subject": "A Message from Brightwheel", "body": "<h1>Your Bill</h1><p>$10</p>"}' -H "Content-Type: application/json" -X POST http://127.0.0.1:$PORT/email
+curl -d '{ "to": "hermannwest@gmail.com", "to_name": "Mr. Fake", "from": "noreply@mybrightwheel.com", "from_name": "Brightwheel", "subject": "A Message from Brightwheel", "body": "<h1>Your Bill</h1><p>$10</p>"}' -H "Content-Type: application/json" -X POST http://127.0.0.1:$PORT/email
 
 # bad email message
 curl -d '{ "to": "bademail", "to_name": "Mr. Fake", "from": "noreply@mybrightwheel.com", "from_name": "Brightwheel", "subject": "A Message from Brightwheel", "body": "<h1>Your Bill</h1><p>$10</p>"}' -H "Content-Type: application/json" -X POST http://127.0.0.1:$PORT/email
+
+# unauth recip
+curl -d '{ "to": "someone@example.com", "to_name": "Mr. Fake", "from": "noreply@mybrightwheel.com", "from_name": "Brightwheel", "subject": "A Message from Brightwheel", "body": "<h1>Your Bill</h1><p>$10</p>"}' -H "Content-Type: application/json" -X POST http://127.0.0.1:$PORT/email
