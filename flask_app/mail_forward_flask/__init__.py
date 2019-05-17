4465f8ea36a3b4f04dca759fd9a06711baf5e5b2
Matthew Flood <hermannwest@gmail.com>
2019-05-17 12:37:04 -0600

    Hook up mailgun in /email endpoint

--- flask_app/mail_forward_flask/__init__.py
+++ flask_app/mail_forward_flask/__init__.py
@@ -3,6 +3,7 @@
 """
 import logging
 import json
+import os
 from flask import Flask
 from flask import jsonify
 from flask import render_template
@@ -12,6 +13,7 @@ from mail_forward_flask.loggingsetup import init_logging
 from mail_forward_flask.message_tools.mf_email import MfEmail
 from mail_forward_flask.message_tools.mf_email import InvalidMfEmailException
 
+from mail_forward_flask.service_provider import ServiceProviderException
 from mail_forward_flask.service_provider.noop import Noop
 from mail_forward_flask.service_provider.mailgun import Mailgun
 
@@ -42,7 +44,7 @@ def email():
     logger.debug("Incoming request content (%s): %s", type(content), content)
     if isinstance(content, str):
         content = json.loads(content)
-        
+
     mf_email = MfEmail()
     try:
         logger.debug("Loading MfEmail")
@@ -51,18 +53,28 @@ def email():
         mf_email.validate()
 
         # build service provider
-        #service_provider = MailGun(api_key=os.environ["MF_MAILGUN_API_KEY"],
-        #                            domain=os.environ["MF_MAILGUN_DOMAIN"]))
-        service_provider = Noop()
+        service_provider = Mailgun(api_key=os.environ["MF_MAILGUN_API_KEY"],
+                                   domain=os.environ["MF_MAILGUN_DOMAIN"])
+        logger.debug("Using provider %s", service_provider)
+        #service_provider = Noop()
+
         service_provider.send_message(mf_email=mf_email)
+        logger.debug("Returning ok")
+        return jsonify({"status": "ok", "provider": str(service_provider)})
+
+    except ServiceProviderException as error:
+        logger.error("ServiceProviderException: %s", error)
+        return jsonify({"status": "error",
+                        "provider": str(service_provider),
+                        "message": str(error),
+                        "details": []})
+
     except InvalidMfEmailException as error:
         logger.error("MFEmail Error: %s Details: %s", error, error.error_list)
         return jsonify({"status": "error",
-                        "message": ["error"],
+                        "message": str(error),
                         "details": error.error_list})
 
-    logger.debug("Returning ok")
-    return jsonify({"status": "ok", "provider": "noop"})
 
 # Uncomment this if you want to invoke the app
 # using:
