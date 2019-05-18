2ca5987b98098b07fb0aa6098355497a2c65ea60
Matthew Flood <hermannwest@gmail.com>
2019-05-17 22:01:42 -0600

    use real from address in first curl

--- flask_app/run_curl.sh
+++ flask_app/run_curl.sh
@@ -5,7 +5,7 @@ PORT=5000
 #PORT=8000
 
 # normal message
-curl -d '{ "to": "hermannwest@gmail.com", "to_name": "Mr. Fake", "from": "noreply@mybrightwheel.com", "from_name": "Brightwheel", "subject": "A Message from Brightwheel", "body": "<h1>Your Bill</h1><p>$10</p>"}' -H "Content-Type: application/json" -X POST http://127.0.0.1:$PORT/email
+curl -d '{ "to": "hermannwest@gmail.com", "to_name": "Mr. Fake", "from": "hermannwest@gmail.com", "from_name": "Brightwheel", "subject": "A Message from Brightwheel", "body": "<h1>Your Bill</h1><p>$10</p>"}' -H "Content-Type: application/json" -X POST http://127.0.0.1:$PORT/email
 
 # bad email message
 curl -d '{ "to": "bademail", "to_name": "Mr. Fake", "from": "noreply@mybrightwheel.com", "from_name": "Brightwheel", "subject": "A Message from Brightwheel", "body": "<h1>Your Bill</h1><p>$10</p>"}' -H "Content-Type: application/json" -X POST http://127.0.0.1:$PORT/email
