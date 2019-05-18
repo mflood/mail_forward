fdd7df4a7c6a359a9591bbffa74f92a7aba70ba3
Matthew Flood <hermannwest@gmail.com>
2019-05-17 22:04:10 -0600

    pass docker.env as arg to docker run

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #docker run --name mail_forward -p 8000:8000 --rm mail_forward:latest
-docker run --name mail_forward -p 8000:8000 --rm -i --log-driver=none -a stdout -a stderr mail_forward:latest
+docker run --name mail_forward -p 8000:8000 --env-file=docker.env --rm -i --log-driver=none -a stdout -a stderr mail_forward:latest
